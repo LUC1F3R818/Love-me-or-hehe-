63b24c69d6cc606efc87daa984a49b953f22b18c
LUC1F3R818 <74072072+LUC1F3R818@users.noreply.github.com>
2020-11-25 00:10:59 +0630

    Add files via upload

---
+++ LUCIFER.sh
@@ -0,0 +1,78 @@
+trap 'bye;' 2
+bye () {
+  echo "If you love me ,I will let you go"
+  
+}
+echo -e "\e[1;31m__________________________________\e[0m" 
+figlet '1UC1F3R'
+
+password='LUCIFER'
+Username='LUCIFER'
+read -p "Username:" Name
+read -sp "Password:" Password
+if [[ $Name == $Username || $Password == $password ]];then
+echo ""
+echo -e "\e[1;31m__________________________________\e[0m" 
+figlet 'Welcome'
+
+else
+echo ""
+echo "\e[1;31m Myanmar young Script kiddies \e[0m" 
+figlet 'wrong password
+KMKL' 
+exit
+fi
+
+clear
+echo "
+⟦M⟧⟦y⟧⟦a⟧⟦n⟧⟦m⟧⟦a⟧⟦r⟧ ⟦y⟧⟦o⟧⟦u⟧⟦n⟧⟦g⟧ ⟦s⟧⟦c⟧⟦r⟧⟦i⟧⟦p⟧⟦t⟧ ⟦k⟧⟦i⟧⟦d⟧⟦d⟧⟦i⟧⟦e⟧⟦s⟧⟦M⟧⟦y⟧⟦a⟧⟦n⟧⟦m⟧⟦a⟧⟦r⟧ ⟦y⟧⟦o⟧⟦u⟧⟦n⟧⟦g⟧ ⟦s⟧⟦c⟧⟦r⟧⟦i⟧⟦p⟧⟦t⟧ ⟦k⟧⟦i⟧⟦d⟧⟦d⟧⟦i⟧⟦e⟧⟦s⟧⟦M⟧⟦y⟧⟦a⟧⟦n⟧⟦m⟧⟦a⟧⟦r⟧ ⟦y⟧⟦o⟧⟦u⟧⟦n⟧⟦g⟧ ⟦s⟧⟦c⟧⟦r⟧⟦i⟧⟦p⟧⟦t⟧ ⟦k⟧⟦i⟧⟦d⟧⟦d⟧⟦i⟧⟦e⟧⟦s⟧⟦M⟧⟦y⟧⟦a⟧⟦n⟧⟦m⟧⟦a⟧⟦r⟧ ⟦y⟧⟦o⟧⟦u⟧⟦n⟧⟦g⟧ ⟦s⟧⟦c⟧⟦r⟧⟦i⟧⟦p⟧⟦t⟧ ⟦k⟧⟦i⟧⟦d⟧⟦d⟧⟦i⟧⟦e⟧⟦s⟧⟦M⟧⟦y⟧⟦a⟧⟦n⟧⟦m⟧⟦a⟧⟦r⟧ ⟦y⟧⟦o⟧⟦u⟧⟦n⟧⟦g⟧ ⟦s⟧⟦c⟧⟦r⟧⟦i⟧⟦p⟧⟦t⟧ ⟦k⟧⟦i⟧⟦d⟧⟦d⟧⟦i⟧⟦e⟧⟦s⟧⟦M⟧⟦y⟧⟦a⟧⟦n⟧⟦m⟧⟦a⟧⟦r⟧ ⟦y⟧⟦o⟧⟦u⟧⟦n⟧⟦g⟧ ⟦s⟧⟦c⟧⟦r⟧⟦i⟧⟦p⟧⟦t⟧ ⟦k⟧⟦i⟧⟦d⟧⟦d⟧⟦i⟧⟦e⟧⟦s⟧⟦M⟧⟦y⟧⟦a⟧⟦n⟧⟦m⟧⟦a⟧⟦r⟧ ⟦y⟧⟦o⟧⟦u⟧⟦n⟧⟦g⟧ ⟦s⟧⟦c⟧⟦r⟧⟦i⟧⟦p⟧⟦t⟧ ⟦k⟧⟦i⟧⟦d⟧⟦d⟧⟦i⟧⟦e⟧⟦s⟧⟦M⟧⟦y⟧⟦a⟧⟦n⟧⟦m⟧⟦a⟧⟦r⟧ ⟦y⟧⟦o⟧⟦u⟧⟦n⟧⟦g⟧ ⟦s⟧⟦c⟧⟦r⟧⟦i⟧⟦p⟧⟦t⟧ ⟦k⟧⟦i⟧⟦d⟧⟦d⟧⟦i⟧⟦e⟧⟦s⟧⟦M⟧⟦y⟧⟦a⟧⟦n⟧⟦m⟧⟦a⟧⟦r⟧ ⟦y⟧⟦o⟧⟦u⟧⟦n⟧⟦g⟧ ⟦s⟧⟦c⟧⟦r⟧⟦i⟧⟦p⟧⟦t⟧ ⟦k⟧⟦i⟧⟦d⟧⟦d⟧⟦i⟧⟦e⟧⟦s⟧⟦M⟧⟦y⟧⟦a⟧⟦n⟧⟦m⟧⟦a⟧⟦r⟧ ⟦y⟧⟦o⟧⟦u⟧⟦n⟧⟦g⟧ ⟦s⟧⟦c⟧⟦r⟧⟦i⟧⟦p⟧⟦t⟧ ⟦k⟧⟦i⟧⟦d⟧⟦d⟧⟦i⟧⟦e⟧⟦s⟧⟦M⟧⟦y⟧⟦a⟧⟦n⟧⟦m⟧⟦a⟧⟦r⟧ ⟦y⟧⟦o⟧⟦u⟧⟦n⟧⟦g⟧ ⟦s⟧⟦c⟧⟦r⟧⟦i⟧⟦p⟧⟦t⟧ ⟦k⟧⟦i⟧⟦d⟧⟦d⟧⟦i⟧⟦e⟧⟦s⟧⟦M⟧⟦y⟧⟦a⟧⟦n⟧⟦m⟧⟦a⟧⟦r⟧ ⟦y⟧⟦o⟧⟦u⟧⟦n⟧⟦g⟧ ⟦s⟧⟦c⟧⟦r⟧⟦i⟧⟦p⟧⟦t⟧ ⟦k⟧⟦i⟧⟦d⟧⟦d⟧⟦i⟧⟦e⟧⟦s⟧⟦M⟧⟦y⟧⟦a⟧⟦n⟧⟦m⟧⟦a⟧⟦r⟧ ⟦y⟧⟦o⟧⟦u⟧⟦n⟧⟦g⟧ ⟦s⟧⟦c⟧⟦r⟧⟦i⟧⟦p⟧⟦t⟧ ⟦k⟧⟦i⟧⟦d⟧⟦d⟧⟦i⟧⟦e⟧⟦s⟧⟦M⟧⟦y⟧⟦a⟧⟦n⟧⟦m⟧⟦a⟧⟦r⟧ ⟦y⟧⟦o⟧⟦u⟧⟦n⟧⟦g⟧ ⟦s⟧⟦c⟧⟦r⟧⟦i⟧⟦p⟧⟦t⟧ ⟦k⟧⟦i⟧⟦d⟧⟦d⟧⟦i⟧⟦e⟧⟦s⟧⟦M⟧⟦y⟧⟦a⟧⟦n⟧⟦m⟧⟦a⟧⟦r⟧ ⟦y⟧⟦o⟧⟦u⟧⟦n⟧⟦g⟧ ⟦s⟧⟦c⟧⟦r⟧⟦i⟧⟦p⟧⟦t⟧ ⟦k⟧⟦i⟧⟦d⟧⟦d⟧⟦i⟧⟦e⟧⟦s⟧⟦M⟧⟦y⟧⟦a⟧⟦n⟧⟦m⟧⟦a⟧⟦r⟧ ⟦y⟧⟦o⟧⟦u⟧⟦n⟧⟦g⟧ ⟦s⟧⟦c⟧⟦r⟧⟦i⟧⟦p⟧⟦t⟧ ⟦k⟧⟦i⟧⟦d⟧⟦d⟧⟦i⟧⟦e⟧⟦s⟧⟦M⟧⟦y⟧⟦a⟧⟦n⟧⟦m⟧⟦a⟧⟦r⟧ ⟦y⟧⟦o⟧⟦u⟧⟦n⟧⟦g⟧ ⟦s⟧⟦c⟧⟦r⟧⟦i⟧⟦p⟧⟦t⟧ ⟦k⟧⟦i⟧⟦d⟧⟦d⟧⟦i⟧⟦e⟧⟦s⟧⟧
+"
+echo -e "\e[1;34m HEY BEAUTIFUL GIRL \e[0m"
+echo -e "\e[1;31m Coder:Call me Pa Pa\e[0m"
+echo -e "\e[1;34m contact me on fb.https://www.facebook.com/profile.php?id=100049074062062\e[0m"
+echo "⟦M⟧⟦y⟧⟦a⟧⟦n⟧⟦m⟧⟦a⟧⟦r⟧ ⟦y⟧⟦o⟧⟦u⟧⟦n⟧⟦g⟧ ⟦s⟧⟦c⟧⟦r⟧⟦i⟧⟦p⟧⟦t⟧ ⟦k⟧⟦i⟧⟦d⟧⟦d⟧⟦i⟧⟦e⟧⟦s⟧⟦M⟧⟦y⟧⟦a⟧⟦n⟧⟦m⟧⟦a⟧⟦r⟧ ⟦y⟧⟦o⟧⟦u⟧⟦n⟧⟦g⟧ ⟦s⟧⟦c⟧⟦r⟧⟦i⟧⟦p⟧⟦t⟧ ⟦k⟧⟦i⟧⟦d⟧⟦d⟧⟦i⟧⟦e⟧⟦s⟧⟦M⟧⟦y⟧⟦a⟧⟦n⟧⟦m⟧⟦a⟧⟦r⟧ ⟦y⟧⟦o⟧⟦u⟧⟦n⟧⟦g⟧ ⟦s⟧⟦c⟧⟦r⟧⟦i⟧⟦p⟧⟦t⟧ ⟦k⟧⟦i⟧⟦d⟧⟦d⟧⟦i⟧⟦e⟧⟦s⟧⟦M⟧⟦y⟧⟦a⟧⟦n⟧⟦m⟧⟦a⟧⟦r⟧ ⟦y⟧⟦o⟧⟦u⟧⟦n⟧⟦g⟧ ⟦s⟧⟦c⟧⟦r⟧⟦i⟧⟦p⟧⟦t⟧ ⟦k⟧⟦i⟧⟦d⟧⟦d⟧⟦i⟧⟦e⟧⟦s⟧⟦M⟧⟦y⟧⟦a⟧⟦n⟧⟦m⟧⟦a⟧⟦r⟧ ⟦y⟧⟦o⟧⟦u⟧⟦n⟧⟦g⟧ ⟦s⟧⟦c⟧⟦r⟧⟦i⟧⟦p⟧⟦t⟧ ⟦k⟧⟦i⟧⟦d⟧⟦d⟧⟦i⟧⟦e⟧⟦s⟧⟦M⟧⟦y⟧⟦a⟧⟦n⟧⟦m⟧⟦a⟧⟦r⟧ ⟦y⟧⟦o⟧⟦u⟧⟦n⟧⟦g⟧ ⟦s⟧⟦c⟧⟦r⟧⟦i⟧⟦p⟧⟦t⟧ ⟦k⟧⟦i⟧⟦d⟧⟦d⟧⟦i⟧⟦e⟧⟦s⟧⟦M⟧⟦y⟧⟦a⟧⟦n⟧⟦m⟧⟦a⟧⟦r⟧ ⟦y⟧⟦o⟧⟦u⟧⟦n⟧⟦g⟧ ⟦s⟧⟦c⟧⟦r⟧⟦i⟧⟦p⟧⟦t⟧ ⟦k⟧⟦i⟧⟦d⟧⟦d⟧⟦i⟧⟦e⟧⟦s⟧⟦M⟧⟦y⟧⟦a⟧⟦n⟧⟦m⟧⟦a⟧⟦r⟧ ⟦y⟧⟦o⟧⟦u⟧⟦n⟧⟦g⟧ ⟦s⟧⟦c⟧⟦r⟧⟦i⟧⟦p⟧⟦t⟧ ⟦k⟧⟦i⟧⟦d⟧⟦d⟧⟦i⟧⟦e⟧⟦s⟧⟦M⟧⟦y⟧⟦a⟧⟦n⟧⟦m⟧⟦a⟧⟦r⟧ ⟦y⟧⟦o⟧⟦u⟧⟦n⟧⟦g⟧ ⟦s⟧⟦c⟧⟦r⟧⟦i⟧⟦p⟧⟦t⟧ ⟦k⟧⟦i⟧⟦d⟧⟦d⟧⟦i⟧⟦e⟧⟦s⟧⟦M⟧⟦y⟧⟦a⟧⟦n⟧⟦m⟧⟦a⟧⟦r⟧ ⟦y⟧⟦o⟧⟦u⟧⟦n⟧⟦g⟧ ⟦s⟧⟦c⟧⟦r⟧⟦i⟧⟦p⟧⟦t⟧ ⟦k⟧⟦i⟧⟦d⟧⟦d⟧⟦i⟧⟦e⟧⟦s⟧⟦M⟧⟦y⟧⟦a⟧⟦n⟧⟦m⟧⟦a⟧⟦r⟧ ⟦y⟧⟦o⟧⟦u⟧⟦n⟧⟦g⟧ ⟦s⟧⟦c⟧⟦r⟧⟦i⟧⟦p⟧⟦t⟧ ⟦k⟧⟦i⟧⟦d⟧⟦d⟧⟦i⟧⟦e⟧⟦s⟧⟦M⟧⟦y⟧⟦a⟧⟦n⟧⟦m⟧⟦a⟧⟦r⟧ ⟦y⟧⟦o⟧⟦u⟧⟦n⟧⟦g⟧ ⟦s⟧⟦c⟧⟦r⟧⟦i⟧⟦p⟧⟦t⟧ ⟦k⟧⟦i⟧⟦d⟧⟦d⟧⟦i⟧⟦e⟧⟦s⟧⟦M⟧⟦y⟧⟦a⟧⟦n⟧⟦m⟧⟦a⟧⟦r⟧ ⟦y⟧⟦o⟧⟦u⟧⟦n⟧⟦g⟧ ⟦s⟧⟦c⟧⟦r⟧⟦i⟧⟦p⟧⟦t⟧ ⟦k⟧⟦i⟧⟦d⟧⟦d⟧⟦i⟧⟦e⟧⟦s⟧⟦M⟧⟦y⟧⟦a⟧⟦n⟧⟦m⟧⟦a⟧⟦r⟧ ⟦y⟧⟦o⟧⟦u⟧⟦n⟧⟦g⟧ ⟦s⟧⟦c⟧⟦r⟧⟦i⟧⟦p⟧⟦t⟧ ⟦k⟧⟦i⟧⟦d⟧⟦d⟧⟦i⟧⟦e⟧⟦s⟧⟦M⟧⟦y⟧⟦a⟧⟦n⟧⟦m⟧⟦a⟧⟦r⟧ ⟦y⟧⟦o⟧⟦u⟧⟦n⟧⟦g⟧ ⟦s⟧⟦c⟧⟦r⟧⟦i⟧⟦p⟧⟦t⟧ ⟦k⟧⟦i⟧⟦d⟧⟦d⟧⟦i⟧⟦e⟧⟦s⟧⟦M⟧⟦y⟧⟦a⟧⟦n⟧⟦m⟧⟦a⟧⟦r⟧ ⟦y⟧⟦o⟧⟦u⟧⟦n⟧⟦g⟧ ⟦s⟧⟦c⟧⟦r⟧⟦i⟧⟦p⟧⟦t⟧ ⟦k⟧⟦i⟧⟦d⟧⟦d⟧⟦i⟧⟦e⟧⟦⟦s⟧"
+echo -e "\e[1;31m__________________________________\e[0m" 
+figlet 'RFEM'
+echo -e "\e[1;31m ILOVEYOU  babe ,do you love me\e[0m "
+
+echo -e "\e[1;31m 1.I Love You too \e[0m"
+echo -e "\e[1;31m 2.I hate You \e[0m"
+echo ""
+
+
+read -p " Answer me 1 or 1~# " act
+echo""
+
+if [ $act = 1 ] || [ $act = 01 ];
+then
+clear
+
+apt install toilet
+
+clear
+echo -e "\e[1;31m Love you too babe \e[0m"
+
+toilet I love you
+
+fi
+
+sleep 1
+x=LOVE YOU
+until [ $x eq I Love you];do
+echo "$x    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU    LOVE YOU "
+done
+
+if [ $act = 2 ] || [ $act = 02 ];
+then
+clear
+
+toilet  FUCK YOU
+fi
+sleep 2
+x=FUCK YOU
+until [ $x eq I LOVE YOU];do
+echo "$x 
+FUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOUFUCK YOU "
+
+done
+trap 2
